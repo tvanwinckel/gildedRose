ec1437f6bd31b7e5fe9d6cc1125897f51e3a90bb
tvanwinckel <vanwinckelthomas@gmail.com>
2024-06-22 16:19:51 +0200

    Extracted equal methods in readable form. Add MIN and MAX quality lvl as a constant.

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -4,6 +4,8 @@ class GildedRose {
     public static final String AGED_BRIE = "Aged Brie";
     public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
     public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    public static final int MAX_ITEM_QUALITY = 50;
+    public static final int MIN_ITEM_QUALITY = 0;
 
     Item[] items;
 
@@ -13,26 +15,26 @@ class GildedRose {
 
     public void updateQuality() {
         for (final Item item : items) {
-            if (!item.name.equals(AGED_BRIE)
-                && !item.name.equals(BACKSTAGE_PASSES)) {
-                if (item.quality > 0) {
-                    if (!item.name.equals(SULFURAS)) {
+            if (!isAgedBrie(item)
+                && !isBackstagePass(item)) {
+                if (item.quality > MIN_ITEM_QUALITY) {
+                    if (!isSulfuras(item)) {
                         item.quality = item.quality - 1;
                     }
                 }
             } else {
-                if (item.quality < 50) {
+                if (item.quality < MAX_ITEM_QUALITY) {
                     item.quality = item.quality + 1;
 
-                    if (item.name.equals(BACKSTAGE_PASSES)) {
+                    if (isBackstagePass(item)) {
                         if (item.sellIn < 11) {
-                            if (item.quality < 50) {
+                            if (item.quality < MAX_ITEM_QUALITY) {
                                 item.quality = item.quality + 1;
                             }
                         }
 
                         if (item.sellIn < 6) {
-                            if (item.quality < 50) {
+                            if (item.quality < MAX_ITEM_QUALITY) {
                                 item.quality = item.quality + 1;
                             }
                         }
@@ -40,27 +42,39 @@ class GildedRose {
                 }
             }
 
-            if (!item.name.equals(SULFURAS)) {
+            if (!isSulfuras(item)) {
                 item.sellIn = item.sellIn - 1;
             }
 
             if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS)) {
+                if (!isAgedBrie(item)) {
+                    if (!isBackstagePass(item)) {
+                        if (item.quality > MIN_ITEM_QUALITY) {
+                            if (!isSulfuras(item)) {
                                 item.quality = item.quality - 1;
                             }
                         }
                     } else {
-                        item.quality = 0;
+                        item.quality = MIN_ITEM_QUALITY;
                     }
                 } else {
-                    if (item.quality < 50) {
+                    if (item.quality < MAX_ITEM_QUALITY) {
                         item.quality = item.quality + 1;
                     }
                 }
             }
         }
+    }
+
+    private static boolean isSulfuras(final Item item) {
+        return item.name.equals(SULFURAS);
+    }
+
+    private static boolean isBackstagePass(final Item item) {
+        return item.name.equals(BACKSTAGE_PASSES);
+    }
+
+    private static boolean isAgedBrie(final Item item) {
+        return item.name.equals(AGED_BRIE);
     }
 }
