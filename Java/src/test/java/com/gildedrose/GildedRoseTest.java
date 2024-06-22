943ac97ae05f93a88e2238c8f0280c70f7944e5c
tvanwinckel <vanwinckelthomas@gmail.com>
2024-06-22 13:03:39 +0200

    Extracted item names into constants.

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -2,6 +2,9 @@ package com.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
+import static com.gildedrose.GildedRose.AGED_BRIE;
+import static com.gildedrose.GildedRose.BACKSTAGE_PASSES;
+import static com.gildedrose.GildedRose.SULFURAS;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -53,96 +56,96 @@ class GildedRoseTest {
 
     @Test
     void updateQualityOfAgedBrie() {
-        final Item item = new Item("Aged Brie", 2, 0);
+        final Item item = new Item(AGED_BRIE, 2, 0);
         final Item[] items = new Item[] {item};
         final GildedRose app = new GildedRose(items);
         app.updateQuality();
 
-        assertThat(item.name).isEqualTo("Aged Brie");
+        assertThat(item.name).isEqualTo(AGED_BRIE);
         assertThat(item.sellIn).isEqualTo(1);
         assertThat(item.quality).isEqualTo(1);
     }
 
     @Test
     void updateQualityOfAgedBrie_sellInExceeded_qualityIncreasesTwiceAsFast() {
-        final Item item = new Item("Aged Brie", 0, 0);
+        final Item item = new Item(AGED_BRIE, 0, 0);
         final Item[] items = new Item[] {item};
         final GildedRose app = new GildedRose(items);
         app.updateQuality();
 
-        assertThat(item.name).isEqualTo("Aged Brie");
+        assertThat(item.name).isEqualTo(AGED_BRIE);
         assertThat(item.sellIn).isEqualTo(-1);
         assertThat(item.quality).isEqualTo(2);
     }
 
     @Test
     void updateQualityOfAgedBrie_qualityCanNotSurpass50() {
-        final Item item = new Item("Aged Brie", 0, 50);
+        final Item item = new Item(AGED_BRIE, 0, 50);
         final Item[] items = new Item[] {item};
         final GildedRose app = new GildedRose(items);
         app.updateQuality();
 
-        assertThat(item.name).isEqualTo("Aged Brie");
+        assertThat(item.name).isEqualTo(AGED_BRIE);
         assertThat(item.sellIn).isEqualTo(-1);
         assertThat(item.quality).isEqualTo(50);
     }
 
     @Test
     void updateQualityOfSulfuras_sellInAndQualityNeverChange() {
-        final Item item = new Item("Sulfuras, Hand of Ragnaros", 20, 80);
+        final Item item = new Item(SULFURAS, 20, 80);
         final Item[] items = new Item[] {item};
         final GildedRose app = new GildedRose(items);
         app.updateQuality();
 
-        assertThat(item.name).isEqualTo("Sulfuras, Hand of Ragnaros");
+        assertThat(item.name).isEqualTo(SULFURAS);
         assertThat(item.sellIn).isEqualTo(20);
         assertThat(item.quality).isEqualTo(80);
     }
 
     @Test
     void updateQualityOfBackstagePass_sellInAbove10_normalIncreaseInQuality() {
-        final Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20);
+        final Item item = new Item(BACKSTAGE_PASSES, 15, 20);
         final Item[] items = new Item[] {item};
         final GildedRose app = new GildedRose(items);
         app.updateQuality();
 
-        assertThat(item.name).isEqualTo("Backstage passes to a TAFKAL80ETC concert");
+        assertThat(item.name).isEqualTo(BACKSTAGE_PASSES);
         assertThat(item.sellIn).isEqualTo(14);
         assertThat(item.quality).isEqualTo(21);
     }
 
     @Test
     void updateQualityOfBackstagePass_sellInBelow11_increaseInQualityTwiceAsFast() {
-        final Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20);
+        final Item item = new Item(BACKSTAGE_PASSES, 10, 20);
         final Item[] items = new Item[] {item};
         final GildedRose app = new GildedRose(items);
         app.updateQuality();
 
-        assertThat(item.name).isEqualTo("Backstage passes to a TAFKAL80ETC concert");
+        assertThat(item.name).isEqualTo(BACKSTAGE_PASSES);
         assertThat(item.sellIn).isEqualTo(9);
         assertThat(item.quality).isEqualTo(22);
     }
 
     @Test
     void updateQualityOfBackstagePass_sellInBelow6_increaseInQualityTriceAsFast() {
-        final Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20);
+        final Item item = new Item(BACKSTAGE_PASSES, 5, 20);
         final Item[] items = new Item[] {item};
         final GildedRose app = new GildedRose(items);
         app.updateQuality();
 
-        assertThat(item.name).isEqualTo("Backstage passes to a TAFKAL80ETC concert");
+        assertThat(item.name).isEqualTo(BACKSTAGE_PASSES);
         assertThat(item.sellIn).isEqualTo(4);
         assertThat(item.quality).isEqualTo(23);
     }
 
     @Test
     void updateQualityOfBackstagePass_sellInExceeded_qualityZero() {
-        final Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 20);
+        final Item item = new Item(BACKSTAGE_PASSES, 0, 20);
         final Item[] items = new Item[] {item};
         final GildedRose app = new GildedRose(items);
         app.updateQuality();
 
-        assertThat(item.name).isEqualTo("Backstage passes to a TAFKAL80ETC concert");
+        assertThat(item.name).isEqualTo(BACKSTAGE_PASSES);
         assertThat(item.sellIn).isEqualTo(-1);
         assertThat(item.quality).isEqualTo(0);
     }
