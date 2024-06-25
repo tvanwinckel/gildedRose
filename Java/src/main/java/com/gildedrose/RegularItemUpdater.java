ca5b605dcd5f2e2d00f983cdb9921796ffd3194e
tvanwinckel <vanwinckelthomas@gmail.com>
2024-06-25 11:01:49 +0200

    Moved over logic for updating regular items.

--- Java/src/main/java/com/gildedrose/RegularItemUpdater.java
+++ Java/src/main/java/com/gildedrose/RegularItemUpdater.java
@@ -1,8 +1,14 @@
 package com.gildedrose;
 
+import static com.gildedrose.ItemUpdater.DEADLINE;
+import static com.gildedrose.ItemUpdater.MIN_ITEM_QUALITY;
+import static java.lang.Math.max;
+
 public class RegularItemUpdater {
 
     public void update(final Item item) {
-
+        final int quality = item.sellIn > DEADLINE ? 1 : 2;
+        item.quality = max(item.quality - quality, MIN_ITEM_QUALITY);
+        item.sellIn = item.sellIn - 1;
     }
 }
